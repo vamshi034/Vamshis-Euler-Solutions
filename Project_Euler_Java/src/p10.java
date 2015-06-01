0856225fb768ffce36e65fd2230a5b8ed57af816
Vamshi034 <vamshi034@yahoo.com>
2015-06-01 19:09:07 -0400

    Slight improvement to algortihm

--- Project_Euler_Java/src/p10.java
+++ Project_Euler_Java/src/p10.java
@@ -7,9 +7,12 @@ public class p10 {
 		
 		//This Algorithm skips over even numbers.
 		for(int i = 5; i < 2000000; i += 2){
-			double limit = Math.ceil(Math.sqrt(i)); //This algorithm only needs to check up to the sqrt of i. 
-			l1: for(double x = 3; x <= limit; x += 1){
-				if(i % x == 0.0){
+			double limit = Math.ceil(Math.sqrt(i)); //This algorithm only needs to check up to the sqrt of i.
+			if(limit % 2 == 0){
+				limit++; //If it's even add one to make it odd so that my algorithm's limit can land on it.
+			}
+			l1: for(double x = 3; x <= limit; x += 2){ //I want to skip all even numbers because even numbers can never
+				if(i % x == 0.0){					   // have odd multiples, and prime numbers have to be odd (except for 2).
 					break l1;
 				}else if(x == limit){
 					answer += i;
